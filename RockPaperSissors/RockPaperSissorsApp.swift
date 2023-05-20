0912144c82d84bd258b13a4b5b36aa1024fb1a0d
Blair Duddy <blairduddy@hotmail.co.uk>
2023-05-20 14:14:34 -0400

    Initial Commit

---
+++ RockPaperSissors/RockPaperSissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperSissorsApp.swift
+//  RockPaperSissors
+//
+//  Created by Blair Duddy on 2023-05-20.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperSissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
